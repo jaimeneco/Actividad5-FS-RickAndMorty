fc2084cdc18409a883eeb862abeb5f0debe6d08e
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2025-05-21 20:27:34 +0200

    up

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Routes, Link } from "react-router";
 export const Header = () => {
     return (
         <header>
-            <img className="logo" src="./img/logo-rickMorty.png"></img>
+            <img className="logo" src="/img/logo-rickMorty.png"></img>
             <nav>
                 <button><Link to="/">Personajes</Link></button>
                 <button><Link to="/locations">Lugares</Link></button>
