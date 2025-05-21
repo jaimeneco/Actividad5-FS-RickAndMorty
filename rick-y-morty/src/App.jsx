09d21547b850513249f4f5e0b9635c0fe21d06bc
jaimeneco <jaime.nebot.colom@alumnos.cei.es>
2025-05-21 20:23:32 +0200

    up

--- rick-y-morty/src/App.jsx
+++ rick-y-morty/src/App.jsx
@@ -8,18 +8,6 @@ import { Episodios } from "./components/Episodios";
 import { Header } from "./components/Header";
 import { Footer } from "./components/Footer";
 
-// const Header = () => {
-//   return (
-//     <header>
-//       <img className="logo" src="./img/logo-rickMorty.png"></img>
-//       <nav>
-//         <button><Link to="/">Personajes</Link></button>
-//         <button><Link to="/locations">Lugares</Link></button>
-//         <button><Link to="/episodes">Episodios</Link></button>
-//       </nav>
-//     </header>
-//   );
-// };
 
 const App = () => {
   return (
